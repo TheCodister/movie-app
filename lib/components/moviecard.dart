b599b60139eb9ae753c46eb819ee43604d7602c4
TheCodister <quangcuber002@gmail.com>
2024-11-08 15:17:30 +0700

    feat: restyle moviecard

--- lib/components/moviecard.dart
+++ lib/components/moviecard.dart
@@ -49,39 +49,49 @@ class Moviecard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20.0),
-      ),
-      margin: const EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 10.0),
-      elevation: 10.0,
-      child: Column(
-        children: <Widget>[
-          Image.network(
-            posterUrl,
-            width: double.maxFinite,
-            fit: BoxFit.cover,
-          ),
-          ListTile(
-            title: Text(
-              title,
-              style:
-                  const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-            ),
-            subtitle: Text(year),
-          ),
-          ElevatedButton(
-            onPressed: () => _addToFavorites(context),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color.fromARGB(255, 255, 131, 173),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        margin: const EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 10.0),
+        elevation: 10.0,
+        child: Column(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                topRight: Radius.circular(20.0),
+              ),
+              child: Image.network(
+                posterUrl,
+                width: double.maxFinite,
+                fit: BoxFit.cover,
               ),
             ),
-            child: const Text('Add to favorite',
-                style: TextStyle(color: Colors.white)),
-          ),
-        ],
+            ListTile(
+              title: Text(
+                title,
+                style: const TextStyle(
+                    fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(year),
+            ),
+            ElevatedButton(
+              onPressed: () => _addToFavorites(context),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 255, 131, 173),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
+              child: const Text('Add to favorite',
+                  style: TextStyle(color: Colors.white)),
+            ),
+            const SizedBox(height: 10.0),
+          ],
+        ),
       ),
     );
   }
